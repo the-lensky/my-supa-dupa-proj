4175514cd9f3c9a9dde392ebf661d74b0ca30483
Nikita Zelensky <nzialensky@gmail.com>
2023-09-14 14:48:49 +0300

    decompose webpack config

---
+++ config/build/types/buildWebpackConfig.ts
@@ -0,0 +1,25 @@
+import { BuildOptions } from './config';
+import webpack from 'webpack';
+import { buildPlugins } from '../buildPlugins';
+import { buildLoaders } from '../buildLoaders';
+import { buildResolvers } from '../buildResolvers';
+
+export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
+
+    const { mode, paths } = options;
+
+    return {
+        mode,
+        entry: paths.entry,
+        output: {
+            filename: '[name].[contenthash].js',
+            path: paths.build,
+            clean: true
+        },
+        plugins: buildPlugins(options),
+        module: {
+            rules: buildLoaders(),
+        },
+        resolve: buildResolvers(),
+    };
+}
