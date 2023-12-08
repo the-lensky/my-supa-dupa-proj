9756149309939b213e907a431fb62c5fe6ae3ae8
Nikita Zelensky <nzialensky@gmail.com>
2023-12-08 14:51:44 +0300

    Add navbar widget

--- src/app/types/global.d.ts
+++ src/app/types/global.d.ts
@@ -1,3 +1,9 @@
+declare module '*.scss' {
+    const content: Record<string, string>;
+    export default content;
+}
+
+
 declare module '*.css' {
     interface IClassNames {
         [className: string]: string;
